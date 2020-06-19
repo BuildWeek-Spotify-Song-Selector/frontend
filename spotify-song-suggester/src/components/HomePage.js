1eb909dcf0e7baeab4fcce076516ceabb39aab52
JoeOs <JoeOs@github.com>
2020-06-18 23:17:32 -0400

    app styles WIP

---
+++ spotify-song-suggester/src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const HomePage = () => {
+    return (
+        <div class="container-fluid">
+<nav class="navbar navbar-light bg-light">
+  <span class="navbar-brand mb-0 h1">Spotify-song-suggester</span>
+</nav>
+        </div>
+    )
+}
+
+export default HomePage
