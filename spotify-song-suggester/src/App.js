9494ef68424becf6c0793c3a7bebdec7938d539c
JoeOs <JoeOs@github.com>
2020-06-22 22:40:14 -0400

    Login form

--- spotify-song-suggester/src/App.js
+++ spotify-song-suggester/src/App.js
@@ -2,30 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './components/HomePage'
-import Navigation from './components/Navigation'
-import Login from './components/LoginForm';
+import Login from './components/Login';
 
-import styled from 'styled-components'
-const ContainerDiv = styled.div`
-  margin:0 auto;
-    padding:0 auto;
-    width: 100%;
-    height: 100%;
-    background-color: #222629;
-`;
 
 
 
 function App () {
   return (
+    <div className="app">
     <Router>
-    <ContainerDiv>
-    <HomePage />
-    </ContainerDiv>
-
-
+    <Login />
     </Router>
+    </div>
+
 
 
   );
--- spotify-song-suggester/src/components/Login.js
+++ spotify-song-suggester/src/components/Login.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-function Login () {
-    return (
-      <div>
-      <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
 
-      </div>
+import './Login.css'
+const Login = () => {
 
-    )
+  const HandleSub = (val) => {
+    alert(JSON.stringify(val));
+    axios.get("", val).post(resp => console.log(resp)).catch(err => console.log(err))
+    
+};
+
+  const initialValues = {
+    username: "",
+    password: ""
+};
+
+
+  const SignupSchema =
+    Yup.object().shape({
+    username: Yup.string().required("add a username"),
+    password: Yup.string().required("add a password")
+  });
+
+  return (
+    <div className="login">
+      <h1>Log In</h1>
+      <Formik {...{initialValues, HandleSub}} 
+      validationSchema={SignupSchema}>
+        {() => (
+          <Form>
+            <div>
+              <label htmlFor="username">Username</label>
+              <Field type="text" id="username" name="username" />
+            </div>
+
+            <div>
+              <label htmlFor="password">Password</label>
+              <Field type="password" id="password" name="password" />
+            </div>  
+            <div>
+              <button type="submit" >Submit</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
 }
+
 
 export default Login;
